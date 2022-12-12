02bf270f8e561b0c3151cf644d94fb9e4e5ee926
Dimi Bibikau <dmitry.bibikov@gmail.com>
2022-12-12 13:18:27 +0100

    init: two class solution ES6

---
+++ classOneES6.js
@@ -0,0 +1,51 @@
+class BaseClass {
+    constructor(value = 0) {
+        this.value = value;
+    }
+
+    plus(...args){
+        this.value = args.reduce((sum, elem) => sum + elem, this.value);
+        return this;
+    }
+
+    minus(...args){
+        this.value = args.reduce((sum, elem) => sum - elem, this.value);
+        return this;
+    }
+
+    multiply(n){
+        this.value = this.value * n;
+        return this;
+    }
+
+    divide(n){
+        this.value = this.value / n;
+        return this;
+    }
+
+    mod(n){
+        this.value = this.value % n;
+        return this;
+    }
+    get(){
+        return this.value;
+    }
+}
+
+class IntBuilder extends BaseClass {
+    constructor(value) {
+        super(value);
+    }
+}
+
+let intBuilder = new IntBuilder(10);
+
+intBuilder
+    .plus(2, 3, 2)
+    .minus(1, 2)
+    .multiply(2)
+    .divide(4)
+    .mod(3)
+    .get()
+
+console.log(intBuilder.value)
