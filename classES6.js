5099defcddf500d39b79f750070ddd73d671e9ff
Dimi Bibikau <dmitry.bibikov@gmail.com>
2022-12-12 13:14:42 +0100

    feat: all the rest of the actions added

--- classES6.js
+++ classES6.js
@@ -11,10 +11,63 @@ class Adder extends BaseClass {
 
     plus(...args){
         this.value = args.reduce((sum, elem) => sum + elem, this.value);
+        return this;
+    }
+}
+class Subtractor extends Adder {
+    constructor(value) {
+        super(value);
+    }
+
+    minus(...args){
+        this.value = args.reduce((sum, elem) => sum - elem, this.value);
+        return this;
     }
 }
 
-class IntBuilder extends Adder {
+class Multiplier extends Subtractor{
+    constructor(value) {
+        super(value);
+    }
+
+    multiply(n){
+        this.value = this.value * n;
+        return this;
+    }
+}
+
+class Divider extends Multiplier{
+    constructor(value) {
+        super(value);
+    }
+
+    divide(n){
+        this.value = this.value / n;
+        return this;
+    }
+}
+
+class Remainder extends Divider {
+    constructor(value) {
+        super(value);
+    }
+
+    mod(n){
+        this.value = this.value % n;
+        return this;
+    }
+}
+
+class Getter extends Remainder{
+    constructor(value) {
+        super(value);
+    }
+
+    get(){
+        return this.value;
+    }
+}
+class IntBuilder extends Getter {
     constructor(value) {
         super(value);
     }
@@ -24,5 +77,10 @@ let intBuilder = new IntBuilder(10);
 
 intBuilder
     .plus(2, 3, 2)
+    .minus(1, 2)
+    .multiply(2)
+    .divide(4)
+    .mod(3)
+    .get()
 
 console.log(intBuilder.value)
